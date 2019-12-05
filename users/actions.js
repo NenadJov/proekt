b4c1f0f51f7eba11bb3afa876d7b9a8c04f78097
NenadJov <52101094+NenadJov@users.noreply.github.com>
2019-12-05 21:57:37 +0100

    new

--- users/actions.js
+++ users/actions.js
@@ -1,4 +1,64 @@
 const connection = require('../database');
+const bcrypt = require('bcryptjs');
+
+createUserQuery = (name, surname, email, age, pass) => {
+    const query = 'INSERT INTO users (Name, Surname, Email, Age, Password) VALUES (?, ?, ?, ?, ?)';
+    return new Promise((resolve, reject) => {
+        connection.query(query, [name, surname, email, age, pass], (error, results, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
+createUser = async (req, res, next) => {
+    const userName = req.body.Name;
+    const userSurname = req.body.Surname;
+    const userEmail = req.body.Email;
+    const userAge = req.body.Age;
+    const userPassword = req.body.Password;
+    try {
+        const passHash = bcrypt.hashSync(userPassword, 10);
+        const user = await createUserQuery(userName, userSurname, userEmail, userAge, passHash);
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+// getUserByEmailQuery = (email) => {
+//     const query = 'SELECT * FROM users WHERE Email = ?';
+//     return new Promise((resolve, reject) => {
+//         connection.query(query, [email], (error, results, fields) => {
+//             if (error) {
+//                 reject(error);
+//             } else {
+//                 resolve(results);
+//             }
+//         });
+//     });
+// };
+
+// loginUser = async (req, res) => {
+//     const email = req.body.Email;
+//     const pass = req.body.Password;
+//     try {
+//         var user = await getUserByEmailQuery(email);
+//         var dbUser = user[0];
+//         const matchPass = bcrypt.compareSync(pass, dbUser.Password);
+//         if (matchPass) {
+//             const token = jwt.sign({ dbUser }, 'abcd', { expiresIn: '1h' });
+//             res.status(200).send(token);
+//         } else {
+//             res.status(401).send('wrong pass');
+//         }
+//     } catch (error) {
+//         res.status(500).send(error.message);
+//     }
+// }; 
 
 getAllUsersQuery = () => {
     const query = 'SELECT * FROM users';
@@ -23,5 +83,6 @@ getAllUsers = async (req, res) => {
 };
 
 module.exports = {
+    createUser,
     getAllUsers
 };
--- users/routes.js
+++ users/routes.js
@@ -4,5 +4,6 @@ const actions = require('./actions');
 const routes = express.Router();
 
 routes.get('/users', actions.getAllUsers);
+routes.post('/users', actions.createUser);
 
 module.exports = routes; 
