37443f162fc5827a8bbf681859a1c70edd6a054e
NenadJov <52101094+NenadJov@users.noreply.github.com>
2019-12-09 21:08:04 +0100

    new

--- users/routes.js
+++ users/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const actions = require('./actions');
+const {emailValidator} = require('../validator');
+const {checkIsOlderThan18} = require('../validator');
+
 
 const routes = express.Router();
 
-routes.post('/users', actions.createUser);
+routes.post('/users', emailValidator, checkIsOlderThan18, actions.createUser);
 routes.post('/login', actions.loginUser);
 routes.get('/users', actions.getAllUsers);
 routes.put('/users/:id', actions.updateUser);
